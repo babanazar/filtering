3298319e8a7892123a2bf30dd5dfb57f5043d99e
Babanazar <g.babanazar@gmail.com>
2021-03-06 02:13:41 +0300

    gitignore added

--- filtering/urls.py
+++ filtering/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,4 +24,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
